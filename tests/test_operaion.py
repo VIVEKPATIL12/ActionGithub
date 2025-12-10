5d009eb9e50d60d5653d85a96359e27b3efd9b04
[VIVEKPATIL12] <[patilvivek3004@gmail.com]>
2025-12-10 23:27:48 +0530

    adding mul and div

--- tests/test_operaion.py
+++ tests/test_operaion.py
@@ -1,4 +1,4 @@
-from src.math_operation import add, subtract
+from src.math_operation import add, subtract, multiply, divide
 import unittest
 
 
@@ -11,3 +11,16 @@ def test_subtract():
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 5) == 0
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(-6, 2) == -3
+    try:
+        divide(5, 0)
+    except ValueError as e:
+        assert str(e) == "Cannot divide by zero"
